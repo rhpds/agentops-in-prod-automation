b201ac7fcef6f2f6725a6f1bff7069403138e00d
rcarrata <rcarratalasanchez@gmail.com>
2026-04-16 18:00:27 +0200

    added grafana fixes per user

--- deployments/grafana/files/consolelink/create-consolelink.sh
+++ deployments/grafana/files/consolelink/create-consolelink.sh
@@ -2,10 +2,14 @@
 
 set -ex
 
-ROUTE=$(oc get route -n grafana grafana-route -ojsonpath='{.status.ingress[0].host}')
+NAMESPACE="${NAMESPACE:-grafana}"
+USERNAME="${USERNAME:-user1}"
+
+ROUTE=$(oc get route -n "${NAMESPACE}" grafana-route -ojsonpath='{.status.ingress[0].host}')
 export ROUTE
 
 cat << EOF | oc apply -f-
 $(cat /mnt/consolelink.yaml.tpl)
+  name: grafana-${USERNAME}
   href: https://${ROUTE}/
 EOF
