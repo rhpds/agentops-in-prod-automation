c5eda9d0148440c5ea610595faf35d662169866c
rcarrata <rcarratalasanchez@gmail.com>
2026-04-16 18:16:11 +0200

    added stuff

--- deployments/grafana/files/consolelink/create-consolelink.sh
+++ deployments/grafana/files/consolelink/create-consolelink.sh
@@ -6,10 +6,6 @@ NAMESPACE="${NAMESPACE:-grafana}"
 USERNAME="${USERNAME:-user1}"
 
 ROUTE=$(oc get route -n "${NAMESPACE}" grafana-route -ojsonpath='{.status.ingress[0].host}')
-export ROUTE
+export ROUTE USERNAME
 
-cat << EOF | oc apply -f-
-$(cat /mnt/consolelink.yaml.tpl)
-  name: grafana-${USERNAME}
-  href: https://${ROUTE}/
-EOF
+envsubst < /mnt/consolelink.yaml.tpl | oc apply -f-
